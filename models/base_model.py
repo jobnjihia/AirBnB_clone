330d61e077bc27c25170bf5940f1a125a1ba2773
jobnjihia <jobnjihia09@gmail.com>
2023-10-17 06:04:21 +0000

    base class

--- models/base_model.py
+++ models/base_model.py
@@ -8,22 +8,40 @@ from datetime import datetime
 import uuid
 
 
-def BaseModel:
+class BaseModel:
     """
     Represents the base class of the AirBnB project
     """
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         """
         initialization of the BaseModel instance
         _*args: list of arguements
         _*kwargs: dict of key-value arguements
         """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
         Return a string representation of Basemodel Instance
         """
-        return "[{}] ({}) {}".
+        return "[{}] ({}) {}"
         format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
@@ -31,6 +49,7 @@ def BaseModel:
         Update the 'updated_at' attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""
+file_storage
+serializes python objects into json objects
+deserializes json objects to python objects
+"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
+
+class FileStorage():
+    """
+    FileStorage class:
+    attributes:
+        __file_path: string - path to the JSON file
+        __objects: dictionary - store all objects by <class name>.id
+
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        public instance method that returns the
+        dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        public instance method that sets in __objects
+        the obj with key <obj class name>.id
+        Variables:
+        ----------
+        key [str] -- key format generated.
+        """
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        public instance method that serializes __objects
+        to the JSON file (path: __file_path).
+        Variables:
+        ----------
+        nw_dct [dict] -- keys and values to build JSON.
+        """
+        nw_dct = {}
+        for key, value in FileStorage.__objects.items():
+            nw_dct[key] = value.to_dict().copy()
+        with open(FileStorage.__file_path, mode='w') as my_file:
+            json.dump(nw_dct, my_file)
+
+    def reload(self):
+        """
+        public instance method that deserializes a JSON
+        file to __objects.
+        """
+        try:
+            with open(FileStorage.__file_path, mode='r') as my_file:
+                nw_dct = json.load(my_file)
+
+            for key, value in nw_dct.items():
+                class_name = value.get('__class__')
+                obj = eval(class_name + '(**value)')
+                FileStorage.__objects[key] = obj
+
+        except FileNotFoundError:
+            pass
